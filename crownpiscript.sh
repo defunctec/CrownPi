fc2267b2791bf557853b773982774810f08ea861
defunctec <defuncteconomics@gmail.com>
2019-03-28 12:25:16 +0000

    Jade 0.13.0 Release
    Improvements to CrownPi, updated to latest version 0.13.0

--- crownpiscript.sh
+++ crownpiscript.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Manual
-# Password change prompt
-echo Please change device password...
-passwd
+# Clear Screen
+clear_screen() {
+     clear
+     echo "  _____                         _____ _ " 
+     echo " / ____|                       |  __ (_)"
+     echo "| |     _ __ _____      ___ __ | |__) | "
+     echo "| |    | '__/ _ \ \ /\ / / '_ \|  ___/ |"
+     echo "| |____| | | (_) \ V  V /| | | | |   | |"
+     echo " \_____|_|  \___/ \_/\_/ |_| |_|_|   |_|"     
+     sleep 3
+     echo Installing...
+     sleep 2
+}
 
 # Software install
 install_dependencies() {
@@ -16,16 +25,9 @@ install_dependencies() {
 
 # Attempt to create 1GB swap ram
 create_swap() {
-    echo Creating swap ram...
-    sudo mkdir -p /var/cache/swap/   
-    sudo dd if=/dev/zero of=/var/cache/swap/myswap bs=1M count=1024
-    sudo chmod 600 /var/cache/swap/myswap
-    sudo mkswap /var/cache/swap/myswap
-    sudo swapon /var/cache/swap/myswap
-    swap_line='/var/cache/swap/myswap   none    swap    sw  0   0'
-    # Add the line only once 
-    sudo grep -q -F "$swap_line" /etc/fstab || echo "$swap_line" | sudo tee --append /etc/fstab > /dev/null
-    cat /etc/fstab
+    echo Adding 1GB Swap
+    sudo sed -i -e 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+    sudo /etc/init.d/dphys-swapfile restart
 }
 
 # Update OS
@@ -38,7 +40,7 @@ update_repos() {
 # Download Crown client (Update link with new client)
 download_package() {
     # Password change prompt
-    echo Getting 0.12.5.3 client
+    echo Getting 0.13.0 MN-PoS client...
     # Create temporary directory
     dir=`mktemp -d`
     if [ -z "$dir" ]; then
@@ -47,7 +49,7 @@ download_package() {
         mkdir -p $dir
     fi
     # Change this later to take latest release version.
-    wget "https://gitlab.crown.tech/crown/crown-core/-/jobs/4986/artifacts/download" -O $dir/crown.zip
+    sudo wget "https://github.com/Crowndev/crown-core/releases/download/v0.13.0/Crown-0.13.0.0-RaspberryPi.zip" -O $dir/crown.zip
 }
 
 # Install Crown client
@@ -61,60 +63,21 @@ install_package() {
 
 # Firewall
 configure_firewall() {
-    echo Setting up firewall
+    echo Setting up firewall...
     sudo ufw allow ssh/tcp
     sudo ufw limit ssh/tcp
     sudo ufw allow 9340/tcp
-    sudo ufw allow 10050
-    sudo ufw allow 53/udp
-    sudo ufw allow 111/udp
-    sudo ufw allow 123/udp
-    sudo ufw allow 443/udp
-    sudo ufw allow 1194/udp
-    sudo ufw allow 8282/udp
-    sudo ufw logging on
-    sudo ufw --force enable
 }
-
-# Crown package
-main() {
-    # Stop crownd (in case it's running)
-    sudo crown-cli stop
-    # Install Packages
-    install_dependencies
-    # Download the latest release
-    download_package
-    # Extract and install
-    install_package
-
-    if [ "$install" = true ] ; then
-        # Create swap to help with sync
-        create_swap
-        # Update Repos
-        update_repos
-        # Create folder structures and configure crown.conf
-        configure_conf
-        # Configure firewall
-        configure_firewall
-    fi
-
-}
-
-handle_arguments "$@"
-main
-
-# Crontab additions 
-    echo Adding to Crontab...
-    (crontab -l 2>/dev/null; echo "@reboot sudo /usr/local/bin/crownd") | crontab -
-    (crontab -l 2>/dev/null; echo "*/30 * * * * restart.sh") | crontab -
 
 # Maintenance scripts
-    echo Downloading scripts and other useful tools, please wait...
+maintenance_scripts() {
+    echo Downloading scripts and other useful tools...
     sudo wget "https://www.dropbox.com/s/kucyc0fupop6vca/crwrestart.sh?dl=0" -O restart.sh | bash && sudo chmod +x restart.sh
-    sudo wget "https://www.dropbox.com/s/hbb7516orhf7saq/update.sh?dl=0" -O update.sh | bash && sudo chmod +x update.sh
     sudo wget "https://www.dropbox.com/s/gq4vxog7riom739/whatsmyip.sh?dl=0" -O whatsmyip.sh | bash && sudo chmod +x whatsmyip.sh
+}
 
-
+# Zabbix Install
+zabbix_install() {
 # Declare variable choice and assign value 4
 echo Would you like to install a Zabbix agent?
 choice=3
@@ -159,10 +122,12 @@ else
         fi
 fi
 done
+}
 
-
+# NordVPN Install
+vpn_install() {
 # Declare variable choice and assign value 4
-echo Please choose your VPN provider...
+echo Please choose a VPN provider...
 choice=4
 # Print to stdout
  echo "1. NordVPN"
@@ -184,6 +149,14 @@ if [ $choice -eq 1 ] ; then
     sudo mv openvpn /etc
     sudo apt-get install openvpn -y
     sudo chmod 755 /etc/openvpn
+    sudo ufw allow 53/udp
+    sudo ufw allow 111/udp
+    sudo ufw allow 123/udp
+    sudo ufw allow 443/udp
+    sudo ufw allow 1194/udp
+    sudo ufw allow 8282/udp
+    sudo ufw logging on
+    sudo ufw --force enable
     echo Please enter your NordVPN username and password, with the username at the top and password below the username.
     read -p "Press enter to continue"
     sudo nano /etc/openvpn/auth.txt
@@ -198,6 +171,14 @@ else
 
         if [ $choice -eq 2 ] ; then
                  echo "You have chosen VPN Area"
+         sudo ufw allow 53/udp
+         sudo ufw allow 111/udp
+         sudo ufw allow 123/udp
+         sudo ufw allow 443/udp
+         sudo ufw allow 1194/udp
+         sudo ufw allow 8282/udp
+         sudo ufw logging on
+         sudo ufw --force enable        
          sudo apt-get install openvpn-systemd-resolved -y
          sudo wget "https://www.dropbox.com/s/m4gxzf0iazri1ht/vpnareainstall.pl?dl=0" -O vpnarea.sh | bash
                  sudo chmod 755 vpnarea.sh
@@ -222,8 +203,61 @@ else
         fi
 fi
 done
+}
 
-sudo crownd
+configure_conf() {
+    echo Setting up crown.conf
+    cd $ROOT
+    sudo mkdir -p /root/.crown
+    sudo mv /root/.crown/crown.conf /root/.crown/crown.bak
+    sudo touch /root/.crown/crown.conf
+    IP=$(curl http://checkip.amazonaws.com/)
+    PW=$(< /dev/urandom tr -dc a-zA-Z0-9 | head -c32;echo;)
+    sudo echo "==========================================================="
+    sudo pwd 
+    echo 'testnet=0' | sudo tee -a /root/.crown/crown.conf
+    echo 'daemon=1' | sudo tee -a /root/.crown/crown.conf 
+    echo 'staking=1' | sudo tee -a /root/.crown/crown.conf
+    echo 'rpcallowip=127.0.0.1' | sudo tee -a /root/.crown/crown.conf 
+    echo 'rpcuser=crowncoinrpc' | sudo tee -a /root/.crown/crown.conf 
+    echo 'rpcpassword='$PW | sudo tee -a /root/.crown/crown.conf 
+    echo 'listen=1' | sudo tee -a /root/.crown/crown.conf 
+    echo 'server=1' | sudo tee -a /root/.crown/crown.conf 
+    echo 'externalip='$IP | sudo tee -a /root/.crown/crown.conf
+    echo 'masternode=1' | sudo tee -a /root/.crown/crown.conf
+    echo 'masternodeprivkey=YOURGENKEYHERE' | sudo tee -a /root/.crown/crown.conf
+    sudo cat /root/.crown/crown.conf
+}
+
+# Crown package
+main() {
+    # Clear screen
+    clear_screen
+    # Stop crownd (in case it's running)
+    sudo crown-cli stop
+    # Install Packages
+    install_dependencies
+    # Download the latest release
+    download_package
+    # Extract and install
+    install_package
+    # Create swap to help with sync
+    create_swap
+    # Update Repos
+    update_repos
+    # Configure firewall
+    configure_firewall
+    # Maintenance Scripts
+    maintenance_scripts
+    # Install Zabbix
+    zabbix_install
+    # Install VPN
+    vpn_install
+    # Create folder structures and configure crown.conf
+    configure_conf
+}
+
+main
 
 # Notes
 echo Please continue with the guide...
